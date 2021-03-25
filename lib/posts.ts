59e0d7773aeb1eaa1ef48e1ca9088970b54869f8
debbysa <deby.silvia23@gmail.com>
2021-03-25 14:59:29 +0700

    convert to ts

--- lib/posts.ts
+++ lib/posts.ts
@@ -23,7 +23,7 @@ export function getSortedPostData() {
     // Combine the data with the id
     return {
       id,
-      ...matterResult.data,
+      ...(matterResult.data as { date: string; title: string }),
     }
   })
   // Sort posts by date
@@ -60,7 +60,7 @@ export function getAllPostIds() {
   })
 }
 
-export async function getPostData(id) {
+export async function getPostData(id: string) {
   const fullPath = path.join(postsDirectory, `${id}.md`)
   const fileContents = fs.readFileSync(fullPath, 'utf8')
 
@@ -75,6 +75,6 @@ export async function getPostData(id) {
   return {
     id,
     contentHtml,
-    ...matterResult.data,
+    ...(matterResult.data as { date: string; title: string }),
   }
 }
