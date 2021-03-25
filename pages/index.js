01a8626bbeec8ec8b585cca75a08035edb38f23f
Deby Silvia Agnes <deby.silvia23@gmail.com>
2021-03-25 14:23:33 +0700

    Try preview (#1)
    * feat(index): add some introduction
    
    * initial commit

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,11 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[My Name is Deby Silvia Agnes]</p>
+        <p>My Name is Deby Silvia Agnes</p>
+        <p>
+          You can call me Deby. This is the first time I learn about next js. I will try to make
+          another app using next js
+        </p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
